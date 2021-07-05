66a3b779c0855ee5a5c6ed76a92bb4b26ce096f5
krizzia19 <krizziamaecainglet.bsit.ucu@gmail.com>
2021-07-05 21:46:45 +0800

    uploaded

---
+++ group7driverlicense/app/src/main/java/com/example/group7driverlicense/DisplayData.java
@@ -0,0 +1,14 @@
+package com.example.group7driverlicense;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DisplayData extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_data);
+    }
+}
